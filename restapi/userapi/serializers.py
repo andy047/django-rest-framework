707fd3bee5cb2e1447e6c9b8e206990bfed4f22e
andyr <andyrodriguezjimenez@gmail.com>
2021-11-18 00:58:02 -0400

    Initial commit

---
+++ restapi/userapi/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+class UserSerializer(serializers.Serializer):
+    id = serializers.ReadOnlyField()
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    username = serializers.CharField()
+    email = serializers.EmailField()
+    password = serializers.CharField()
+
+    def create(self, validate_data):
+        
+
