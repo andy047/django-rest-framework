29b3baf96295a7b844df27f283435ae8d823e7c4
andyr <andyrodriguezjimenez@gmail.com>
2021-11-18 01:35:26 -0400

    rest framework install

--- restapi/restapi/urls.py
+++ restapi/restapi/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from userapi.api import UserAPI
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/create_user/',UserAPI.as_view(),name="api_create_user"),
 ]
--- restapi/userapi/serializers.py
+++ restapi/userapi/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 class UserSerializer(serializers.Serializer):
@@ -9,5 +10,19 @@ class UserSerializer(serializers.Serializer):
     password = serializers.CharField()
 
     def create(self, validate_data):
-        
+        instance = User()
+        instance.first_name = validate_data.get('first_name')
+        instance.last_name = validate_data.get('last_name')
+        instance.username = validate_data.get('username')
+        instance.email = validate_data.get('email')
+        instance.set_password(validate_data.get('password'))
+        instance.save()
+        return instance
+
+    def validate_username(self,data):
+        users = User.objects.filter(username=data)
+        if len(users) != 0:
+            raise serializers.ValidationError("User already exists! Type a new one")
+        else:
+            return data
 
